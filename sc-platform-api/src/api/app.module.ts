f33c77586581f0c7688bb792461b6b081b98258d
VlO-OlV <linichenko.vanya@gmail.com>
2025-01-10 00:03:17 +0200

    enhancement #1: created auth entities

--- sc-platform-api/src/api/app.module.ts
+++ sc-platform-api/src/api/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from '../config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      envFilePath: [`.${process.env.NODE_ENV}.env`, '.env'],
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
--- sc-platform-api/src/main.ts
+++ sc-platform-api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './api/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get<number>('port');
+
+  await app.listen(port);
+
+  console.info(`Server started on http://localhost:${port}/\n`);
 }
 bootstrap();
